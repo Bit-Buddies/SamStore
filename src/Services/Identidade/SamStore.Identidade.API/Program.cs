4a1a16304ff70ae9f159b23d81ecf63f029e442f
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-01-14 20:49:39 -0300

    Configuração do Swagger

--- src/Services/Identidade/SamStore.Identidade.API/Program.cs
+++ src/Services/Identidade/SamStore.Identidade.API/Program.cs
@@ -19,7 +19,16 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "SamStore Identity API",
+        Description = "Essa API está sendo desenvolvida para prática e estudos em paralelo ao curso de ASP.NET Core Enterprise Applications do Desenvolvedor.IO",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact() { Name = "Samuel de Carvalho", Email = "samueldcarvalho99@gmail.com" },
+        License = new Microsoft.OpenApi.Models.OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")}
+    });
+});
 
 var app = builder.Build();
 
