55bf060ed67a47ad8bac1889862a770c42115184
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-01-14 20:49:31 -0300

    Criação de controller de Auth e dois endpoints, um para Register e outro para Login. Implementado sistemática de registrar e logar

---
+++ src/Services/Identidade/SamStore.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SamStore.Identidade.API.Models;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace SamStore.Identidade.API.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterViewModel registerData)
+        {
+            if(!ModelState.IsValid) return BadRequest();
+
+            IdentityUser user = new IdentityUser()
+            {
+                UserName = registerData.Email,
+                Email = registerData.Email,
+                EmailConfirmed = true
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerData.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Logar(LoginViewModel loginData)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            SignInResult result = await _signInManager.PasswordSignInAsync(loginData.Email, loginData.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ src/Services/Identidade/SamStore.Identidade.API/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SamStore.Identidade.API.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
