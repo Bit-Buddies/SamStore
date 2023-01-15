929f7324cd10d0e28f51146bc1f672ecf672b428
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-01-14 22:01:00 -0300

    Devolvendo JWT e dados para o client

--- src/Services/Identidade/SamStore.Identidade.API/Controllers/AuthController.cs
+++ src/Services/Identidade/SamStore.Identidade.API/Controllers/AuthController.cs
@@ -1,6 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using SamStore.Identidade.API.Extensions;
 using SamStore.Identidade.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace SamStore.Identidade.API.Controllers
@@ -11,11 +18,13 @@ namespace SamStore.Identidade.API.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly IdentitySettings _identitySettings;
 
-        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IOptions<IdentitySettings> identitySettings)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _identitySettings = identitySettings.Value;
         }
 
         [HttpPost("register")]
@@ -35,14 +44,14 @@ namespace SamStore.Identidade.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, isPersistent: false);
-                return Ok();
+                return Ok(await GenerateJwt(user.Email));
             }
 
             return BadRequest();
         }
 
         [HttpPost("authenticate")]
-        public async Task<IActionResult> Logar(LoginViewModel loginData)
+        public async Task<IActionResult> Login(LoginViewModel loginData)
         {
             if (!ModelState.IsValid) return BadRequest();
 
@@ -50,10 +59,62 @@ namespace SamStore.Identidade.API.Controllers
 
             if (result.Succeeded)
             {
-                return Ok();
+                return Ok(await GenerateJwt(loginData.Email));
             }
 
             return BadRequest();
         }
+
+        private async Task<UserData> GenerateJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            claims.Add(new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach(var role in roles) 
+            {
+                claims.Add(new Claim("role", role));
+            }
+
+            ClaimsIdentity identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(_identitySettings.Secret);
+
+            SecurityToken token = tokenHandler.CreateToken(new SecurityTokenDescriptor()
+            {
+                Issuer = _identitySettings.Issuer,
+                Audience = _identitySettings.Audience,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_identitySettings.HoursToExpire),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            string encodedToken = tokenHandler.WriteToken(token);
+
+            UserData userData = new UserData()
+            {
+                AccessToken = encodedToken,
+                HoursToExpire = TimeSpan.FromHours(_identitySettings.HoursToExpire).TotalSeconds,
+                UserToken = new UserToken()
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UserClaim() { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return userData;
+        }
+
+        private static long ToUnixEpochDate(DateTime date) =>
+            (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
---
+++ src/Services/Identidade/SamStore.Identidade.API/Models/UserData.cs
@@ -0,0 +1,22 @@
+﻿namespace SamStore.Identidade.API.Models
+{
+    public class UserData
+    {
+        public string AccessToken { get; set; }
+        public double HoursToExpire { get; set; }
+        public UserToken UserToken { get; set; }
+    }
+
+    public class UserToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UserClaim> Claims { get; set; }
+    }
+
+    public class UserClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+}
