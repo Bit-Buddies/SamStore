cd6a948350351a791a4a7f438ebf4e3a06911d96
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-01-16 18:21:09 -0300

    Configuração do Prettier e Eslint no projeto

--- src/Clients/Web/SamStoreWeb/src/app/app.module.ts
+++ src/Clients/Web/SamStoreWeb/src/app/app.module.ts
@@ -5,19 +5,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from "@angular/material/icon"
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { ToolbarComponent } from './components/main/layout/toolbar/toolbar.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatBadgeModule } from '@angular/material/badge';
 
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './pages/main/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ToolbarComponent, LoginComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatTooltipModule,
+    MatBadgeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
