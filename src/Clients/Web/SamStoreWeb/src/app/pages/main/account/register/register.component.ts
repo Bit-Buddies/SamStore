2ded6bcb147a038e2c478c7aedca322702c9f870
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-01-18 21:55:20 -0300

    Implementação de validações e info de erro no form de registrar usuário

--- src/Clients/Web/SamStoreWeb/src/app/pages/main/account/register/register.component.ts
+++ src/Clients/Web/SamStoreWeb/src/app/pages/main/account/register/register.component.ts
@@ -1,38 +1,58 @@
-import { AuthService } from './../../../../services/auth.service';
-import { RegisterUserData } from './../../../../models/register-user-data';
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from "./../../../../services/auth.service";
+import { RegisterUserData } from "./../../../../models/register-user-data";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
-export class RegisterComponent implements OnInit, AfterViewInit {
+export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
-  registerUserData!: RegisterUserData;
   hidePassword = true;
 
-  constructor(
-    private _formBuilder: FormBuilder,
-    private _authService: AuthService
-  ) {}
+  private _registerData!: RegisterUserData;
+
+  constructor(private _formBuilder: FormBuilder, private _authService: AuthService) {}
+
+  registerUser() {
+    if (!this.registerForm.dirty || this.registerForm.invalid) return;
+
+    this._registerData = { ...this._registerData, ...this.registerForm.value };
+
+    this._authService.registerUser(this._registerData);
+  }
+
+  getControlError(controlName: string, errorCode: string): boolean {
+    return this.registerForm.controls[controlName].hasError(errorCode);
+  }
+
+  mustmatchValidator() {
+    const passwordControl = this.registerForm.get("password")!;
+    const repeatPasswordControl = this.registerForm.get("repeatPassword")!;
+
+    if (!repeatPasswordControl.dirty && !repeatPasswordControl.touched) {
+      return;
+    }
+
+    if (repeatPasswordControl.errors && !repeatPasswordControl.hasError("passwordMismatched")) {
+      return;
+    }
+
+    if (passwordControl.value === repeatPasswordControl.value) {
+      return repeatPasswordControl.setErrors(null);
+    }
+
+    return repeatPasswordControl.setErrors({ passwordMismatched: true });
+  }
 
   ngOnInit(): void {
     this.registerForm = this._formBuilder.group({
-      email: [''],
-      password: [''],
-      repeatPassword: [''],
+      name: ["", Validators.compose([Validators.required, Validators.minLength(3)])],
+      email: ["", Validators.compose([Validators.required, Validators.email])],
+      password: ["", Validators.compose([Validators.required, Validators.minLength(6)])],
+      repeatPassword: ["", Validators.compose([Validators.required])],
     });
-  }
-
-  ngAfterViewInit(): void {}
-
-  public registerUser() {
-    if (!this.registerForm.dirty || !this.registerForm.valid) return;
-
-    Object.assign({}, this.registerUserData, ...this.registerForm.value);
-
-    this._authService.registerUser(this.registerUserData);
   }
 }
