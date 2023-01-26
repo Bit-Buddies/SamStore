f12d91487fc289352175b91acc918ceb65a775d7
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-01-25 23:06:25 -0300

    Correção no tipo de DateTime

--- src/BuildingBlocks/SamStore.Core/Infrastructure/Data/Extensions/DefaultTableConfigurations.cs
+++ src/BuildingBlocks/SamStore.Core/Infrastructure/Data/Extensions/DefaultTableConfigurations.cs
@@ -23,6 +23,11 @@ namespace SamStore.Core.Infrastructure.Data.Extensions
             {
                 prop.SetColumnType("DECIMAL(65,4)");
             }
+
+            foreach (var prop in properties.Where(p => p.ClrType == typeof(DateTime)))
+            {
+                prop.SetColumnType("DATETIME");
+            }
         }
 
         public static void UseDefaultContextConfiguration(this DbContextOptionsBuilder optionsBuilder)
