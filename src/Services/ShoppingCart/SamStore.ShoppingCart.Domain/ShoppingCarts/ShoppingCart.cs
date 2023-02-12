335d334588a3bdd5e97c8d6e2b4af24206dc2f08
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-02-12 02:19:05 -0300

    feat(shoppingcart): criacao das principais entidades

---
+++ src/Services/ShoppingCart/SamStore.ShoppingCart.Domain/ShoppingCarts/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using SamStore.Core.Domain;
+using SamStore.Core.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamStore.ShoppingCart.Domain.ShoppingCarts
+{
+    public class ShoppingCart : Entity, IAggregateRoot
+    {
+        public Guid CostumerId { get; private set; }
+        public decimal Total { get; private set; }
+        public ICollection<ShoppingCartItem> Items { get; private set; } = new List<ShoppingCartItem>();
+
+        protected ShoppingCart() { }
+        public ShoppingCart(Guid costumerId)
+        {
+            CostumerId = costumerId;
+        }
+    }
+}
---
+++ src/Services/ShoppingCart/SamStore.ShoppingCart.Domain/ShoppingCarts/ShoppingCartItem.cs
@@ -0,0 +1,29 @@
+﻿using SamStore.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamStore.ShoppingCart.Domain.ShoppingCarts
+{
+    public class ShoppingCartItem : Entity
+    {
+        public Guid ProductId { get; private set; }
+        public Guid ShoppingCartId { get; private set; }
+        public string Name { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+        public string Image { get; private set; }
+
+        public ShoppingCartItem(Guid productId, Guid shoppingCartId, string name, int quantity, decimal price, string image)
+        {
+            ProductId = productId;
+            ShoppingCartId = shoppingCartId;
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            Image = image;
+        }
+    }
+}
