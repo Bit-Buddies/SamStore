5c1eee4e82547bbec600a9a804450950119d49bb
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-02-12 02:55:58 -0300

    feat(soppingcart/api): added principal controller

--- src/Services/ShoppingCart/SamStore.ShoppingCart.API/Controllers/ShoppingCartController.cs
+++ src/Services/ShoppingCart/SamStore.ShoppingCart.API/Controllers/ShoppingCartController.cs
@@ -1,10 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyNetQ;
+using Microsoft.AspNetCore.Mvc;
+using SamStore.ShoppingCart.Domain.ShoppingCarts;
 using SamStore.WebAPI.Core.API.Controllers;
 
 namespace SamStore.ShoppingCart.API.Controllers
 {
-    [Route("api/shopping_cart")]
+    [Route("api/cart")]
     public class ShoppingCartController : MainController
     {
+        [HttpGet]
+        public async Task<ActionResult<ShoppingCartCostumer>> GetCartCostumer()
+        {
+            return CustomResponse();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddItem(ShoppingCartItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut("{productId}")]
+        public async Task<IActionResult> UpdateItem(Guid productId, ShoppingCartItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> DeleteItem(Guid productId)
+        {
+            return CustomResponse();
+        }
     }
 }
