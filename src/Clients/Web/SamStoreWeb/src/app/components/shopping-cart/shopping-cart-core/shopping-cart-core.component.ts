be2fab5945bc8966e55c5122dd68131e16f67f9e
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-04-11 14:09:44 -0300

    style(web): change color set from toastrs

--- src/Clients/Web/SamStoreWeb/src/app/components/shopping-cart/shopping-cart-core/shopping-cart-core.component.ts
+++ src/Clients/Web/SamStoreWeb/src/app/components/shopping-cart/shopping-cart-core/shopping-cart-core.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { ModalDialogComponent } from 'src/app/utils/modal-dialog';
+import { Component } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
+import { ToastrService } from "ngx-toastr";
+import { ModalDialogComponent } from "src/app/utils/modal-dialog";
 
 @Component({
-  selector: 'app-shopping-cart-core',
-  templateUrl: './shopping-cart-core.component.html',
-  styleUrls: ['./shopping-cart-core.component.scss']
+  selector: "app-shopping-cart-core",
+  templateUrl: "./shopping-cart-core.component.html",
+  styleUrls: ["./shopping-cart-core.component.scss"],
 })
 export class ShoppingCartCoreComponent extends ModalDialogComponent {
   public items: any[] = [];
 
   constructor(public dialogRef: MatDialogRef<ShoppingCartCoreComponent>) {
     super();
-   }
+  }
 }
