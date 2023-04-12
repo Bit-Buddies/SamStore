d7e8d8e6d39b5fe80f61eefe54643178c13a4e5d
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-04-11 23:11:27 -0300

    feat(web): is logged method in account service

--- src/Clients/Web/SamStoreWeb/src/app/services/account.service.ts
+++ src/Clients/Web/SamStoreWeb/src/app/services/account.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from "@angular/core";
 export class AccountService {
   private _currentUserName = "current-user";
 
-  getCurrentUser(): UserData | null {
+  public getCurrentUser(): UserData | null {
     const userJson = localStorage.getItem(this._currentUserName);
 
     if (userJson == null) return null;
@@ -15,7 +15,7 @@ export class AccountService {
     return JSON.parse(userJson) as UserData;
   }
 
-  setCurrentUser(userData: UserData) {
+  public setCurrentUser(userData: UserData) {
     let user = this.getCurrentUser();
 
     if (user != null) {
@@ -27,7 +27,11 @@ export class AccountService {
     localStorage.setItem(this._currentUserName, userJson);
   }
 
-  removeCurrentUser() {
+  public removeCurrentUser() {
     localStorage.removeItem(this._currentUserName);
+  }
+
+  public isLogged(): boolean {
+    return this.getCurrentUser() != null;
   }
 }
