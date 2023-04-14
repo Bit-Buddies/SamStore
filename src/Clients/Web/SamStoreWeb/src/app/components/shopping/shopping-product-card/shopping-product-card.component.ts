60754fb2f568d2c768cd2116283646c9445034de
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-04-14 01:19:04 -0300

    feat(web): changes toastr after favorite

--- src/Clients/Web/SamStoreWeb/src/app/components/shopping/shopping-product-card/shopping-product-card.component.ts
+++ src/Clients/Web/SamStoreWeb/src/app/components/shopping/shopping-product-card/shopping-product-card.component.ts
@@ -1,6 +1,7 @@
 import { AccountService } from "src/app/services/account.service";
 import { ProductDTO } from "./../../../models/Products/product.DTO";
 import { Component, Input } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
     selector: "app-shopping-product-card",
@@ -11,19 +12,32 @@ export class ShoppingProductCardComponent {
     @Input() public product?: ProductDTO;
     public isFavorite: boolean = false;
 
-    constructor(private _accountService: AccountService) { }
+    constructor(private _accountService: AccountService, private _toastrService: ToastrService) {}
 
     public favoriteProduct() {
-      if(!this._accountService.isLogged()) {
-        this._accountService.callLoginModal().subscribe({
-          next: (success) => {
-            if(success) {
-              this.isFavorite = !this.isFavorite;
-            }
-          }
-        })
-      }else{
-        this.isFavorite = !this.isFavorite;
-      }
+        if (!this._accountService.isLogged()) {
+            this._accountService.callLoginModal().subscribe({
+                next: (success) => {
+                    if (success) {
+                        this.isFavorite = !this.isFavorite;
+
+                        this.showFavoriteToastrMessage;
+                    }
+                },
+            });
+
+            return;
+        } else {
+            this.isFavorite = !this.isFavorite;
+        }
+
+        if (this.isFavorite) this.showFavoriteToastrMessage();
+    }
+
+    private showFavoriteToastrMessage(){
+      this._toastrService.success(`product ${this.product?.name} was favorited`, undefined, {
+        positionClass: 'toast-bottom-center',
+        timeOut: 2000
+      });
     }
 }
