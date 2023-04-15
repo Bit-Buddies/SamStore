0e136dc58d0015b76bbd4fd19957319535ddc1f2
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-04-15 00:55:34 -0300

    feat(web): requisition to get product by id in product details page

--- src/Clients/Web/SamStoreWeb/src/app/pages/main/product/product-details/product-details.component.ts
+++ src/Clients/Web/SamStoreWeb/src/app/pages/main/product/product-details/product-details.component.ts
@@ -1,11 +1,48 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { lastValueFrom } from "rxjs";
 import { ProductDTO } from "src/app/models/Products/product.DTO";
+import { CatalogControllerService } from "src/app/services/controllers/catalog-controller.service";
 
 @Component({
 	selector: "app-product-details",
 	templateUrl: "./product-details.component.html",
 	styleUrls: ["./product-details.component.scss"],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
 	public product?: ProductDTO;
+
+	constructor(
+		private _catalogService: CatalogControllerService,
+		private _activatedRoute: ActivatedRoute,
+		private _toastrService: ToastrService,
+		private _router: Router
+	) {}
+
+	ngOnInit(): void {
+		const productId = this._activatedRoute.snapshot.paramMap.get("productId");
+
+		if (!productId) {
+			this._router.navigate(["/home"]);
+			return;
+		}
+
+		lastValueFrom(this._catalogService.getById(productId))
+			.then((product: ProductDTO) => (this.product = product))
+			.catch((err) => {
+				if (!!err.errors)
+					err.errors.Mensagens.forEach((er: string) => {
+						this._toastrService.error(er, undefined, {
+							positionClass: "toast-bottom-right",
+						});
+					});
+				else
+					this._toastrService.error("An error has ocourred", undefined, {
+						positionClass: "toast-bottom-right",
+					});
+
+				this._router.navigate(["/home"]);
+			});
+	}
 }
--- src/Clients/Web/SamStoreWeb/src/app/services/controllers/catalog-controller.service.ts
+++ src/Clients/Web/SamStoreWeb/src/app/services/controllers/catalog-controller.service.ts
@@ -5,14 +5,18 @@ import { Injectable } from "@angular/core";
 import { ApiServiceEnum, BaseApiService } from "../abstractions/base.service";
 
 @Injectable({
-  providedIn: "root",
+	providedIn: "root",
 })
 export class CatalogControllerService extends BaseApiService {
-  constructor(private _httpClient: HttpClient) {
-    super("Catalog", ApiServiceEnum.Catalog);
-  }
+	constructor(private _httpClient: HttpClient) {
+		super("Catalog", ApiServiceEnum.Catalog);
+	}
 
-  getAllProducts(): Observable<Array<ProductDTO>> {
-    return this._httpClient.get<Array<ProductDTO>>(`${this._baseURL}/products`);
-  }
+	getAllProducts(): Observable<Array<ProductDTO>> {
+		return this._httpClient.get<Array<ProductDTO>>(`${this._baseURL}/products`);
+	}
+
+	getById(productId: string): Observable<ProductDTO> {
+		return this._httpClient.get<ProductDTO>(`${this._baseURL}/products/${productId}`);
+	}
 }
