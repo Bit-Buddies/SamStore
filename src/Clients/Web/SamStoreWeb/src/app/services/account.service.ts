853119140525b167744b6f64136c1f829a244b4e
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-04-15 04:05:54 -0300

    style(web): shopping cart button clear change

--- src/Clients/Web/SamStoreWeb/src/app/services/account.service.ts
+++ src/Clients/Web/SamStoreWeb/src/app/services/account.service.ts
@@ -6,45 +6,44 @@ import { DialogService } from "../utils/dialog-service";
 import { LoginModalComponent } from "../components/authentication/login-modal/login-modal.component";
 
 @Injectable({
-    providedIn: "root",
+	providedIn: "root",
 })
 export class AccountService {
-    private _userCookieToken = "CURRENT_USER";
+	private _userCookieToken = "CURRENT_USER";
 
-    constructor(private _cookieService: CookieService, private _dialogService: DialogService) {}
+	constructor(private _cookieService: CookieService, private _dialogService: DialogService) {}
 
-    public getCurrentUser(): UserData | null {
-        const userJson = this._cookieService.get(this._userCookieToken);
+	public getCurrentUser(): UserData | null {
+		const userJson = this._cookieService.get(this._userCookieToken);
 
-        if (userJson == null || userJson == "") return null;
+		if (userJson == null || userJson == "") return null;
 
-        return JSON.parse(userJson) as UserData;
-    }
+		return JSON.parse(userJson) as UserData;
+	}
 
-    public setCurrentUser(userData: UserData) {
-        let user = this.getCurrentUser();
+	public setCurrentUser(userData: UserData) {
+		let user = this.getCurrentUser();
 
-        if (user != null) {
-            this.removeCurrentUser();
-        }
+		if (user != null) {
+			this.removeCurrentUser();
+		}
 
-        const userJson = JSON.stringify(userData);
+		const userJson = JSON.stringify(userData);
 
-        this._cookieService.set(this._userCookieToken, userJson);
-    }
+		this._cookieService.set(this._userCookieToken, userJson);
+	}
 
-    public removeCurrentUser() {
-        this._cookieService.delete(this._userCookieToken);
-    }
+	public removeCurrentUser() {
+		this._cookieService.delete(this._userCookieToken);
+	}
 
-    public isLogged(): boolean {
-        return !!this.getCurrentUser();
-    }
+	public isLogged(): boolean {
+		return !!this.getCurrentUser();
+	}
 
-    public callLoginModal(): Observable<boolean> {
-        return this._dialogService.genericDialog<LoginModalComponent, any, boolean>(LoginModalComponent, {
-            width: "340px",
-            height: "350px",
-        });
-    }
+	public callLoginModal(): Observable<boolean> {
+		return this._dialogService.genericDialog<LoginModalComponent, any, boolean>(LoginModalComponent, {
+			panelClass: "xs-dialog",
+		});
+	}
 }
