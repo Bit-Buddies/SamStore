c57aaa85964739b841eda302603263449581489f
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-04-15 00:38:14 -0300

    style(web): using data from product object to fill page

--- src/Clients/Web/SamStoreWeb/src/app/models/Products/product.DTO.ts
+++ src/Clients/Web/SamStoreWeb/src/app/models/Products/product.DTO.ts
@@ -1,11 +1,12 @@
 import { ProductRatingDTO } from "./product-rating.DTO";
 
 export interface ProductDTO {
-  id: string;
-  name: string;
-  description: string;
-  value: number;
-  image: string;
-  quantity: number;
-  productRating: ProductRatingDTO;
+	id: string;
+	name: string;
+	description: string;
+	value: number;
+	image: string;
+	quantity: number;
+	ref: string;
+	productRating: ProductRatingDTO;
 }
--- src/Clients/Web/SamStoreWeb/src/app/pages/main/product/product-details/product-details.component.ts
+++ src/Clients/Web/SamStoreWeb/src/app/pages/main/product/product-details/product-details.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { ProductDTO } from "src/app/models/Products/product.DTO";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+	selector: "app-product-details",
+	templateUrl: "./product-details.component.html",
+	styleUrls: ["./product-details.component.scss"],
 })
 export class ProductDetailsComponent {
-
+	public product?: ProductDTO;
 }
