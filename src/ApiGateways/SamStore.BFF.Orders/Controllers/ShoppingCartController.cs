5919770b1f994368e81f555fd46cb151e8bc64a5
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-04-20 22:52:04 -0300

    feat(bff_order/api): controller shopping cart

---
+++ src/ApiGateways/SamStore.BFF.Orders/Controllers/ShoppingCartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SamStore.WebAPI.Core.API.Controllers;
+
+namespace SamStore.BFF.Orders.Controllers
+{
+    [Route("api/[controller]")]
+    public class ShoppingCartController : MainController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetCustomerCart()
+        {
+            return CustomResponse();
+        }
+
+        [HttpGet("items/quantity")]
+        public async Task<IActionResult> GetCustomerCartItemsQuantity()
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCustomerCart()
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> ClearCustomerCart()
+        {
+            return CustomResponse();
+        }
+    }
+}
