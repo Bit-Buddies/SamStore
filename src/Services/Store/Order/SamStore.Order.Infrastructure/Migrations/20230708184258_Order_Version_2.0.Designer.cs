b6234f240adc8be5bd32083b469b45c50db94545
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-07-08 15:43:30 -0300

    feat(order/infra): migrations add 2.0

--- src/Services/Store/Order/SamStore.Order.Infrastructure/Migrations/20230708184258_Order_Version_2.0.Designer.cs
+++ src/Services/Store/Order/SamStore.Order.Infrastructure/Migrations/20230708184258_Order_Version_2.0.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SamStore.Order.Infrastructure.Contexts;
 
@@ -10,9 +11,10 @@ using SamStore.Order.Infrastructure.Contexts;
 namespace SamStore.Order.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderContext))]
-    partial class OrderContextModelSnapshot : ModelSnapshot
+    [Migration("20230708184258_Order_Version_2.0")]
+    partial class Order_Version_20
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/Store/Order/SamStore.Order.Infrastructure/Migrations/20230708184258_Order_Version_2.0.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SamStore.Order.Infrastructure.Migrations
+{
+    public partial class Order_Version_20 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "voucher");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "voucher",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
