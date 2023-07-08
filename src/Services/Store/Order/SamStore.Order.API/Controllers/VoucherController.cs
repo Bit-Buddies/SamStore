6190f47348056ae192ff05c9683564e17b47e285
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-07-08 16:07:36 -0300

    feat(order/api): voucher controller and get by key endpoint

--- src/Services/Store/Order/SamStore.Order.API/Controllers/VoucherController.cs
+++ src/Services/Store/Order/SamStore.Order.API/Controllers/VoucherController.cs
@@ -1,18 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SamStore.Order.Application.Interfaces;
 using SamStore.Order.Application.Models;
 using SamStore.WebAPI.Core.API.Controllers;
+using System.Net;
 
 namespace SamStore.Order.API.Controllers
 {
-    [Authorize]
     [Route("api/v1/[controller]")]
     public class VoucherController : MainController
     {
-        [HttpGet("{key}")]
-        public ActionResult<VoucherDTO> GetByKey(string key)
-        {
+        private readonly IVoucherQueries _voucherQueries;
 
+        public VoucherController(IVoucherQueries voucherQueries)
+        {
+            _voucherQueries = voucherQueries;
+        }
+
+        /// <summary>
+        /// Get voucher by key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [HttpGet("{key}")]
+        [ProducesResponseType(typeof(VoucherDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<VoucherDTO>> GetByKey(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                return NotFound();
+
+            VoucherDTO voucher = await _voucherQueries.GetByKey(key);
+
+            if (voucher == null)
+                return NotFound();
+
+            return CustomResponse(voucher);
         }
     }
 }
