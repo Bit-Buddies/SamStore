301b9c976927a3a2e1dfe6f318d5ee53cf927c8b
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-09-03 13:12:43 -0300

    feat(front): navbar fixed default

--- src/Clients/Web/SamStoreWeb/src/app/app.component.ts
+++ src/Clients/Web/SamStoreWeb/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { provideEnvironmentNgxMask, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <app-toolbar id="navbar" [validateShowNavbar]="true"></app-toolbar>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: [ './app.component.scss'],
   providers: [ provideNgxMask() ]
 })
 export class AppComponent {}
