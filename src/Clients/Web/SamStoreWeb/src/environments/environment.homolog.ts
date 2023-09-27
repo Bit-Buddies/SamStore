c7470acc68faa7b7d56c146f137d154aa3cc941f
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-09-26 22:09:12 -0300

    compile(backend): docker compose catalog api service

---
+++ src/Clients/Web/SamStoreWeb/src/environments/environment.homolog.ts
@@ -0,0 +1,7 @@
+import { IEnvironmentSettings } from "./_interface";
+
+export const environment : IEnvironmentSettings = {
+    AUTHENTICATION_API_BASE_URL: "http://localhost:5274/api/v1/",
+    CATALOG_API_BASE_URL: "http://samstore-catalog-api-homolog:5229/api/v1/",
+    BFF_ORDERS_API_BASE_URL: "http://localhost:5159/api/v1/"
+}
