824a3b3dcf7b0a77ed02f389f8565d06356d0749
Samuel de Carvalho <samueldcarvalho99@gmail.com>
2023-09-27 18:47:40 -0300

    feat(backend): setup auto migrate on shopping cart service

--- src/Services/Store/ShoppingCart/SamStore.ShoppingCart.API/Program.cs
+++ src/Services/Store/ShoppingCart/SamStore.ShoppingCart.API/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using SamStore.ShoppingCart.API.Configurations;
+using SamStore.ShoppingCart.Infrastructure.Contexts;
 using SamStore.WebAPI.Core.API.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,10 +10,13 @@ builder.Services.AddServiceConfiguration(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI();
+
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var context = scope.ServiceProvider.GetRequiredService<ShoppingCartContext>();
+    context.Database.Migrate();
 }
 
 app.UseCors("CorsPolicy");
